8ef3bfb3e8ae39c2e7424da864c30c8f2874d01d
Ismaila Hassan <ismailah28@gmail.com>
2020-06-10 14:14:57 +0100

    feat: add documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import createError from 'http-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+const swaggerDocument = require('../swagger.json');
 
 import { default as baseRouter } from './routes';
 
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', baseRouter);
 
 // catch 404 and forward to error handler
